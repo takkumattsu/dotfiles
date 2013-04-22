cb9c0c0fcca6cced1a017961c0cd7111c370c4f3
TakkuMattsu <takku.mattsu@gmail.com>
2013-04-23 03:54:08 +0900

    Fix Windows copy error

--- setup.bat
+++ setup.bat
@@ -2,8 +2,7 @@
 
 set VIM_RUNTIME_DIR=%USERPROFILE%\vimfiles
 set BUNDLE_DIR=%VIM_RUNTIME_DIR%\bundle
-set VIMRC=%USERPROFILE%\_vimrc
-set VIM_DIR=%USERPROFILE%\_vim
+set VIMRC=%USERPROFILE%\.vimrc
 
 for /f "tokens=*" %%i in ('cd') do set CWD=%%i
 
@@ -12,17 +11,13 @@ for /f "tokens=*" %%i in ('cd') do set CWD=%%i
 :: main
 
 :: set backup list 
-set  CHECK_TARGET=%VIMRC% %VIM_DIR% %VIM_RUNTIME_DIR%
-for %%i in (%CHECK_TARGET%) do (
- echo %%i
+set  CHECK_TARGET="%VIMRC%" "%VIM_RUNTIME_DIR%"
+for %%i in (%CHECK_TARGET%) do ( ^
  call :backup %%i
 )
 
-:: create symbolic link
-:: mklink is support after VISTA.
-
 :: create directory
-mkdir %BUNDLE_DIR%
+mkdir "%BUNDLE_DIR%"
 
 :: Install neobundle
 :: Please set git path
@@ -37,20 +32,29 @@ mkdir %BUNDLE_DIR%
 :: <https://code.google.com/p/msysgit/downloads/list?q=net+installer>
 set GIT="YOUR GIT PATH"
 set GIT="C:\Program Files (x86)\Git\cmd\git.exe"
-%GIT% clone git://github.com/Shougo/neobundle.vim.git %BUNDLE_DIR%\neobundle.vim
+%GIT% clone git://github.com/Shougo/neobundle.vim.git "%BUNDLE_DIR%\neobundle.vim"
 
-:: cretae symbolic link
-mklink %VIMRC% %CWD%\.vimrc
+:: create symbolic link
+:: mklink is support after VISTA.
+:: If mklink is not support,
+:: use copy command.
+mklink "%VIMRC%" "%CWD%\.vimrc"
+::copy /Y "%CWD%\.vimrc" "%VIMRC%" 
 
+echo Finish!
+pause
 exit 0
 
 ::=====================================
 :: backup function
 :backup
 setlocal
- :: YYYYMMDD
+
+ echo backup for %1 
+ :: date format is YYYYMMDD
  set date=%DATE:/=%
- :: HHMMSS
+ 
+ :: tm format is HHMMSS
  set tm=%time:~0,8%
  set tm=%tm::=%
  set tm=%tm: =0%
@@ -58,20 +62,21 @@ setlocal
  set BACKUP=%date%%tm%
 
  :: backup
- if exist "%1" (
-  xcopy /i /e %1 %1.%BACKUP%.bk
+ if exist %1 (
+  echo %1
+  xcopy /I /Y %1 %1_%BACKUP%
  )
 
  :: remove (first)
- if exist "%1" (
+ if exist %1 (
   del /q %1
  )
 
  :: del command can not delete the directory.
- if exist "%1" ( 
-  rmdir /s /q "%1"
+ if exist %1 (
+  rmdir /s /q %1
  )
- 
+
 endlocal
 exit /b 0
 ::backup end
